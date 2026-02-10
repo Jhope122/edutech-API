8a57e782b047951087c537a6c93c3399ba7f7b79
Jhope122 <Giselegomes0100@gmail.com>
2026-02-10 16:28:04 -0300

    :arrow_up:feat: add mocked database model

---
+++ src/model/database.js
@@ -0,0 +1,34 @@
+// Cursos disponíveis
+export const cursos = [
+  { id: "1", curso: "Psicologia", turno: "Manhã" },
+  { id: "2", curso: "Direito", turno: "Tarde" },
+  { id: "3", curso: "Administração", turno: "Noite" }
+];
+
+// Alunos
+export const alunos = [
+  { id: "1", aluno: "Ana Silva", cpf: "111.111.111-11", idCurso: "1" },
+  { id: "2", aluno: "Carlos Souza", cpf: "222.222.222-22", idCurso: "2" },
+  { id: "3", aluno: "Mariana Lima", cpf: "333.333.333-33", idCurso: "3" },
+  { id: "4", aluno: "João Pedro", cpf: "444.444.444-44", idCurso: "1" },
+  { id: "5", aluno: "Fernanda Alves", cpf: "555.555.555-55", idCurso: "2" },
+  { id: "6", aluno: "Lucas Rocha", cpf: "666.666.666-66", idCurso: "3" },
+  { id: "7", aluno: "Beatriz Santos", cpf: "777.777.777-77", idCurso: "1" },
+  { id: "8", aluno: "Rafael Costa", cpf: "888.888.888-88", idCurso: "2" },
+  { id: "9", aluno: "Juliana Martins", cpf: "999.999.999-99", idCurso: "3" },
+  { id: "10", aluno: "Pedro Henrique", cpf: "000.000.000-00", idCurso: "1" }
+];
+
+// Matrículas (referências por IDs)
+export const matriculas = [
+  { id: "1", idAluno: "1", idCurso: "1" },
+  { id: "2", idAluno: "2", idCurso: "2" },
+  { id: "3", idAluno: "3", idCurso: "3" },
+  { id: "4", idAluno: "4", idCurso: "1" },
+  { id: "5", idAluno: "5", idCurso: "2" },
+  { id: "6", idAluno: "6", idCurso: "3" },
+  { id: "7", idAluno: "7", idCurso: "1" },
+  { id: "8", idAluno: "8", idCurso: "2" },
+  { id: "9", idAluno: "9", idCurso: "3" },
+  { id: "10", idAluno: "10", idCurso: "1" }
+];
